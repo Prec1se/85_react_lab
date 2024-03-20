5f3ac29daedb587b03db446502f77592a5b1e4cb
Phoenix <phoenix@pop-os.localdomain>
2024-03-21 00:04:27 +0600

    update

--- src/App.js
+++ src/App.js
@@ -10,16 +10,14 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 function App() {
   return (
     <div>
-      {/* <BrowserRouter>
+      <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Home />}/>
-          <Route path="/login" element={<Login />}/>
-          <Route path="/register" element={<Register />}/>
-          
+          <Route path="/" element={<AddButton />}/>
+          <Route path="/taskmanager" element={<TaskManager />}/>  
         </Routes>
-      </BrowserRouter> */}
+      </BrowserRouter>
       {/* <AddButton /> */}
-      <TaskManager/>
+      {/* <TaskManager/> */}
     </div>
   );
 }
